be62451b81ae855bde1f001bd20a02c289f7d826
msiroilem <twjones86@gmail.com>
2021-08-31 22:14:01 -0400

    movies displaying on Create New Movie page when added, need to figure out how to get Search to display .get results and also need to be able to delete movies

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from './Search'
 
-export default function Home(onChange) {
-  // const [searchquery, setStateQuery]
+export default function Home() {
+  const [searchquery, setStateQuery] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+
+  const getSearchResults = async () => {}
 
   return (
     <div>
