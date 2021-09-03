ddc517260329122a8fdf1314586d2356dedb4f9d
msiroilem <twjones86@gmail.com>
2021-09-03 12:55:36 -0400

    deleted Home content

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,5 @@
 import React from 'react'
 
 export default function Home() {
-  return (
-    <section>
-      <p>Welcome!</p>
-      <p>To!</p>
-    </section>
-  )
+  return <section></section>
 }
